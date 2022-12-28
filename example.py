3a97a333c47077b1dbd7a86149e677e71411bb0c
Jed Gore <jedgore@facteus-admins-MacBook-Pro.local>
2022-12-28 15:12:15 -0500

    initial commit

--- example.py
+++ example.py
@@ -1,38 +1,38 @@
 from objects import *
 
 schema_name = "test_schema"
-db_entry = {
-    "entity_id": 1,
-    "identifier": "WMT",
-    "description": "Walmart Inc.",
+db_map = {
+    "entity_id": 0,
+    "identifier": "",
+    "description": "",
     "attribute_1": [
         {
-            "attribute_1_id": 1,
-            "identifier": "WMT",
+            "attribute_1_id": 0,
+            "identifier": "",
             "attribute_1_subsidiary_1": [
                 {
-                    "attributes_1_subsidiary_1_id": 1,
-                    "identifier": "z_42E8ME0198_MO_OS_SamsClubNetSalesincludingFuel_4",
-                    "description": "Sam's Club net sales (including fuel), mm",
+                    "attributes_1_subsidiary_1_id": 0,
+                    "identifier": "",
+                    "description": "",
                 },
             ],
         }
     ],
     "attributes_2": [
         {
-            "attributes_2_id": 1,
-            "identifier": 1,
-            "description": 1,
+            "attributes_2_id": 0,
+            "identifier": "",
+            "description": "",
         }
     ],
     "attributes_3": [
         {
-            "attributes_3_id": 1,
-            "identifier": 1,
-            "description": 1,
+            "attributes_3_id": 0,
+            "identifier": "",
+            "description": "",
         }
     ],
 }
 
 en = Entity()
-en.create_schema(schema_name, db_entry)
+en.create_schema(schema_name, db_map)
--- objects.py
+++ objects.py
@@ -2,7 +2,9 @@ class Entity:
     def __init__(self):
         pass
 
-    def create_schema(self):
+    def create_schema(self, schema_name, db_map):
+        schema_name = schema_name
+        db_map = db_map
         return
 
     def delete_schema(self):
